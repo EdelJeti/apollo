017d9a5c2d9fd740b738ae1214301886b411309d
EdelJeti <jt@gmx.de>
2020-01-20 22:45:35 +0100

    rename 3

--- rename.py
+++ rename.py
@@ -3,11 +3,11 @@ import os
 
 path = os.chdir("/home/user/images/heic")
 
-i = 1145
+i = 1
 
 for file in os.listdir(path):
     
-    new_file_name = "pic{}.jpg".format(i)
+    new_file_name = "img{}.jpg".format(i)
     
     os.rename(file, new_file_name)
     
